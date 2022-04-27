93310d32371b5a49a95b15b6ad584689b969b3fe
kamcio420 <kamcio420@gmail.com>
2022-04-27 21:38:17 +0200

    Ruch paletki

--- TableTenisConsole.cpp
+++ TableTenisConsole.cpp
@@ -1,17 +1,68 @@
 ﻿#include <iostream>
+#include <windows.h>
 
 using namespace std;
 
-class Ball
+enum Kierunek
 {
-private:
-    int x, y;
-    int orgX, orgY;
+    lewo = -1,
+    prawo = 1
+};
+
+class Paletka
+{
+public:
+
+    void ZmienKierunek(int kierunek_lew, int kierunek_prawo)
+    {
+        if (GetAsyncKeyState(kierunek_lew))
+        {
+            kierunek = lewo;
+        }
+        else if (GetAsyncKeyState(kierunek_prawo))
+        {
+            kierunek = prawo;
+        }
+    }
+
+    void Ruch()
+    {
+        if (Pozycja < 11 && Pozycja > -1)
+        {
+            Pozycja += (int)kierunek;
+        }
+    }
+
+    void Wyswietl()
+    {
+        for (int i = 0; i < Pozycja; i++)
+        {
+            std::cout << " ";
+        }
+
+        std::cout << "-";
+    }
+
+    Kierunek kierunek = prawo;
+    int Pozycja = 3;
 };
 
 int main()
 {
-    cout << "Hello World!\n";
+    Paletka p = {};
+
+    while (true)
+    {
+        // czekaj 250 milisekund
+        Sleep(250);
+        system("cls");
+
+        p.ZmienKierunek('A', 'D');
+        p.Ruch();
+        p.Wyswietl();
+    }
 }
+
+
 
 
