cbcd6c265e0bc1079bcb6bd284f9bca3c39f9cb6
kamcio420 <kamcio420@gmail.com>
2022-04-27 22:07:56 +0200

    Drugi gracz *nie dziala*

--- TableTenisConsole.cpp
+++ TableTenisConsole.cpp
@@ -47,9 +47,62 @@ public:
     int Pozycja = 3;
 };
 
+class Paletka2
+{
+public:
+
+    void ZmienKierunek2(int kierunek_lew, int kierunek_prawo)
+    {
+        if (GetAsyncKeyState(kierunek_lew))
+        {
+            kierunek = lewo;
+        }
+        else if (GetAsyncKeyState(kierunek_prawo))
+        {
+            kierunek = prawo;
+        }
+    }
+
+    void Ruch2()
+    {
+        if (Pozycja < 11 && Pozycja > -1)
+        {
+            Pozycja += (int)kierunek;
+        }
+    }
+
+    void Wyswietl2()
+    {
+        for (int i = 0; i < Pozycja; i++)
+        {
+            std::cout << " ";
+        }
+
+        std::cout << "-";
+    }
+
+    Kierunek kierunek = prawo;
+    int Pozycja = 3;
+};
+
+
+class Plansza
+{
+public:
+    void PustePole()
+    {
+        for (int i = 0; i < 15; i++)
+        {
+            cout << "\n";
+        }
+    }
+};
+
 int main()
 {
-    Paletka p = {};
+    Paletka p1 = {};
+    Paletka2 p2 = {};
+    Plansza pl;
 
     while (true)
     {
@@ -57,9 +110,15 @@ int main()
         Sleep(250);
         system("cls");
 
-        p.ZmienKierunek('A', 'D');
-        p.Ruch();
-        p.Wyswietl();
+        p1.ZmienKierunek('A', 'D');
+        p1.Ruch();
+        p1.Wyswietl();
+        pl.PustePole();
+        p2.ZmienKierunek2('VK_L', 'VK_R');
+        p2.Ruch2();
+        p2.Wyswietl2();
+       
+
     }
 }
 
